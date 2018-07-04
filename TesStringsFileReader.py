39c0ff21b16fa5bae17dc7e9637c74358c939b5d
Old Lu <40810939+CoderOldLu@users.noreply.github.com>
2018-07-04 10:40:36 +0800

    First Commit

---
+++ TesStringsFileReader.py
@@ -0,0 +1,10 @@
+import struct
+
+f = open('D:\\PythonProject\\TesStringsFileEditor\\hearthfires_english.strings', 'rb')
+count = f.read(4)
+dataSize = f.read(4)
+
+print("数据长度：%d" % struct.unpack("I", count))
+print("数据块大小：%d" % struct.unpack("I", dataSize))
+
+
