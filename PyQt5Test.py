2740663974f9006c82683b111a2fa400bf2c16ae
Old Lu <zhijin_lm@126.com>
2018-07-04 17:33:33 +0800

    Add System Icon

--- PyQt5Test.py
+++ PyQt5Test.py
@@ -1,11 +1,26 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Message box')
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, '提示', '您真的要退出程序吗?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-    sys.exit(app.exec())
+    ex = Example()
+    sys.exit(app.exec_())
